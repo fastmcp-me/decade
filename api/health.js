9d39e9a39997c8749e0f436b03289d440f793a93
ndkasndakn <svehiphop2020@gmail.com>
2026-01-14 02:20:33 +0100

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
+  res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.status(200).end(
+  res.end(
     JSON.stringify({
       ok: true,
       service: "refund.decide.fyi",
